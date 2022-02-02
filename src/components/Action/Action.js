481d9df7ff4d3fb27ebdbff79ecbb399ecc0294c
Silvia Martinez <sil@silvia.com>
2022-02-02 20:57:06 +0100

    Update Action component and Add

---
+++ src/components/Action/Action.js
@@ -0,0 +1,15 @@
+const Action = () => {
+  return (
+    <>
+      <a href="call" className="call">
+        Call
+      </a>
+
+      <a href="call" className="hang active">
+        Hang
+      </a>
+    </>
+  );
+};
+
+export default Action;
---
+++ src/components/Actions/Actions.js
@@ -0,0 +1,15 @@
+import Display from "../Display/Display";
+import Action from "../Action/Action";
+
+const Actions = () => {
+  return (
+    <>
+      <div className="actions">
+        <Display />
+        <Action />
+      </div>
+    </>
+  );
+};
+
+export default Actions;
