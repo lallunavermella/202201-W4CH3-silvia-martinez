254449f61c576eb0005943225e530fedf4a0b8de
Silvia Martinez <sil@silvia.com>
2022-02-02 20:17:58 +0100

    Updat component Key and Keyboard

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import "./App.css";
+import Keyboard from "./components/Keyboard/Keyboard";
 
 function App() {
+  const clickNumber = () => {};
+
   return (
     <div className="container">
       <span className="message">Calling...</span>
       <main className="phone">
-        <div className="keyboard-container">
-          <ol className="keyboard">
-            <li>
-              <button className="key">1</button>
-            </li>
-            <li>
-              <button className="key">2</button>
-            </li>
-            <li>
-              <button className="key">3</button>
-            </li>
-            <li>
-              <button className="key">4</button>
-            </li>
-            <li>
-              <button className="key">5</button>
-            </li>
-            <li>
-              <button className="key">6</button>
-            </li>
-            <li>
-              <button className="key">7</button>
-            </li>
-            <li>
-              <button className="key">8</button>
-            </li>
-            <li>
-              <button className="key">9</button>
-            </li>
-            <li>
-              <button className="key">0</button>
-            </li>
-            <li>
-              <button className="key big">delete</button>
-            </li>
-          </ol>
-        </div>
+        <Keyboard action={clickNumber} />
         <div className="actions">
           <span className="number">667359961</span>
 
